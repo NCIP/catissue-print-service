e4ff7fe1b86a80f4f5ab5110d0bbb1eb15c6b9fe
Sreenath Nampally <sreenath.nampally@nih.gov>
2013-09-25 10:37:39 -0400

    Add source code headers to Java source files
    Run the following shell code to add a basic BSD legal notice to
    the top of source files while preserving the Byte-Order-Mark:
    
    git ls-files -- '*.java' |
    while read f; do
      sed -i '1s|^\(\xEF\xBB\xBF\)\?|&/*L\
     *  Copyright Washington University in St. Louis\
     *  Copyright SemanticBits\
     *  Copyright Persistent Systems\
     *  Copyright Krishagni\
     *\
     *  Distributed under the OSI-approved BSD 3-Clause License.\
     *  See http://ncip.github.com/catissue-print-service/LICENSE.txt for details.\
     */\
    \
    |' "$f"
    done

--- src/edu/wustl/catissuecore/webservice/util/LabelResourceHandler.java
+++ src/edu/wustl/catissuecore/webservice/util/LabelResourceHandler.java
@@ -1,3 +1,13 @@
+/*L
+ *  Copyright Washington University in St. Louis
+ *  Copyright SemanticBits
+ *  Copyright Persistent Systems
+ *  Copyright Krishagni
+ *
+ *  Distributed under the OSI-approved BSD 3-Clause License.
+ *  See http://ncip.github.com/catissue-print-service/LICENSE.txt for details.
+ */
+
 /**
  * This class reads the LabelResource.properties
  *  which has the field names to be displayed on the label
